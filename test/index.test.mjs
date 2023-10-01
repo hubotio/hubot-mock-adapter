037f2435d5a9426dcb746f34e701c5f0a3912ae9
Joey Guerra <github@joeyguerra.com>
2023-09-30 23:15:43 -0500

    feat: Update to Hubot > 9 BREAKING_CHANGE: Upgrade to async/await

--- test/index.test.mjs
+++ test/index.test.mjs
@@ -2,66 +2,41 @@ import { describe, it, beforeEach, afterEach } from 'node:test'
 import assert from 'node:assert/strict'
 import Robot from 'hubot/src/robot.js'
 import { TextMessage } from 'hubot/src/message.js'
-import MockAdapter from '../index.js'
-import Module from 'module'
-
-// Hook into Node's module loading system to return a mock from require
-let originalRequire = Module.prototype.require
-const hookModuleToReturnMockFromRequire = (module, mock) => {
-  Module.prototype.require = function() {
-    if (arguments[0] === module) {
-      return mock;
-    }
-    return originalRequire.apply(this, arguments)
-  }
-}
-const restoreModuleRequire = () => {
-  Module.prototype.require = originalRequire
-}
 
 describe('Eddie the shipboard computer', () => {
   let robot
   let user
   let adapter
 
-  beforeEach((t, done) => {
-    // hook into require to return our mock adapter
-    hookModuleToReturnMockFromRequire('hubot-mock-adapter', MockAdapter)
-
+  beforeEach(async () => {
     // create new robot, without http, using the mock adapter
     robot = new Robot('hubot-mock-adapter', false, 'Eddie')
-
-    // load adapter the specified adapter during creation
-    // Note: you can also specify a file to load an adapter from
-    robot.loadAdapter().then(() => {
-      robot.adapter.on('connected', () => {
-        // create a user
-        user = robot.brain.userForId("1", {
-          name: 'tester',
-          room: '#test-room'
-        })
-        adapter = robot.adapter
-        done()
+    await robot.loadAdapter('./index.js')
+    robot.adapter.on('connected', () => {
+      // create a user
+      user = robot.brain.userForId("1", {
+        name: 'tester',
+        room: '#test-room'
       })
-      robot.run()
-    }).catch(console.error)
+      adapter = robot.adapter
+    })
+    await robot.run()
   })
 
   afterEach(() => {
     robot.shutdown()
-    restoreModuleRequire()
   })
 
-  it('responds when greeted with a Direct Message', (t, done) => {
-    robot.respond(/Computer\!/, msg => {
-      msg.reply('Why hello there!')
+  it('responds when greeted with a Direct Message', async () => {
+    robot.respond(/Computer\!/, async msg => {
+      await msg.reply('Why hello there!')
     })
-
-    // here's where the magic happens!
+    let wasCalled = false
     robot.adapter.on('reply', (envelope, strings) => {
       assert.deepEqual(strings[0], 'Why hello there!')
-      done()
+      wasCalled = true
     })
-    robot.receive(new TextMessage(user, '@Eddie Computer!'))
+    await robot.receive(new TextMessage(user, '@Eddie Computer!'))
+    assert.deepEqual(wasCalled, true)
   })
 })
